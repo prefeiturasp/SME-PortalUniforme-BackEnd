4641cea1ca36b6e5ba20e34554d7beda03354a01
alcfernandes <alessandro.c.fernandes@gmail.com>
2020-02-05 13:56:59 -0300

    feat(proponentes): Cria URL para retornar lista de tipos de documento
    Cria a URL /tipos-documento/ que retorna a lista de documentos
    cadastrados.

---
+++ sme_uniforme_apps/proponentes/tests/test_tipo_documento_serializer.py
@@ -0,0 +1,16 @@
+import pytest
+
+from ..api.serializers.tipo_documento_serializer import TipoDocumentoSerializer
+
+pytestmark = pytest.mark.django_db
+
+
+def test_tipo_documento_serializer(tipo_documento):
+    tipo_documento_serializer = TipoDocumentoSerializer(tipo_documento)
+
+    assert tipo_documento_serializer.data is not None
+    assert tipo_documento_serializer.data['criado_em']
+    assert tipo_documento_serializer.data['alterado_em']
+    assert tipo_documento_serializer.data['nome']
+    assert tipo_documento_serializer.data['obrigatorio']
+    assert tipo_documento_serializer.data['uuid']
