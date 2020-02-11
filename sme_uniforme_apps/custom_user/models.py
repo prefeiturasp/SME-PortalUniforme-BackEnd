141e396061e592a8f102a6bba05cd318227b33c0
alcfernandes <alessandro.c.fernandes@gmail.com>
2020-02-11 14:42:17 -0300

    feat(usuarios): Desativa envio informações de acesso por e-mail para o proponentes
    Essa funcionalidade foi desativada para aguardar a conclusão da funcionalidade de login do proponente.

--- sme_uniforme_apps/custom_user/models.py
+++ sme_uniforme_apps/custom_user/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django_use_email_as_username.models import BaseUser, BaseUserManager
 
-from .tasks import enviar_email_info_acesso
-
 
 class User(BaseUser):
     objects = BaseUserManager()
@@ -14,4 +12,6 @@ class User(BaseUser):
         novo_usuario = cls.objects.create(email=email, first_name=nome, )
         novo_usuario.set_password(senha)
         novo_usuario.save()
-        enviar_email_info_acesso.delay(email=email, nome=nome, senha=senha)
+
+        # TODO Reativar envio de e-mail de acesso quando estiver concluida a funcionanlidade de login do proponente
+        # enviar_email_info_acesso.delay(email=email, nome=nome, senha=senha)
