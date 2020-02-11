45222c32c840355cb6861b929b8857518bdd1921
alcfernandes <alessandro.c.fernandes@gmail.com>
2020-02-11 14:36:09 -0300

    feat(usuarios): Envia informações de acesso por e-mail para o proponente inscrito
    Após a inscrição de um proponente o sistema envia um e-mail com as
    informações de acesso.

--- sme_uniforme_apps/custom_user/models.py
+++ sme_uniforme_apps/custom_user/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django_use_email_as_username.models import BaseUser, BaseUserManager
 
+from .tasks import enviar_email_info_acesso
+
 
 class User(BaseUser):
     objects = BaseUserManager()
@@ -8,7 +10,8 @@ class User(BaseUser):
                                    help_text="Identificar se o cadastro de usuário já foi validado")
 
     @classmethod
-    def cria_usuario(cls, email, nome, senha_inicial):
+    def cria_usuario(cls, email, nome, senha):
         novo_usuario = cls.objects.create(email=email, first_name=nome, )
-        novo_usuario.set_password(senha_inicial)
+        novo_usuario.set_password(senha)
         novo_usuario.save()
+        enviar_email_info_acesso.delay(email=email, nome=nome, senha=senha)
--- sme_uniforme_apps/proponentes/services.py
+++ sme_uniforme_apps/proponentes/services.py
@@ -7,4 +7,4 @@ def cnpj_esta_bloqueado(cnpj):
 
 
 def cria_usuario_novo_proponente(proponente):
-    User.cria_usuario(email=proponente.email, nome=proponente.responsavel, senha_inicial=proponente.protocolo)
+    User.cria_usuario(email=proponente.email, nome=proponente.responsavel, senha=proponente.protocolo)
