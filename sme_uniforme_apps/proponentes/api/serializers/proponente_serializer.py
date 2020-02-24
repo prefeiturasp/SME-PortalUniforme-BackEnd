8a3218e2e72229437f14a158ebc409f6145cf1bd
anderson89marques <andersonoanjo18@gmail.com>
2020-02-24 19:31:50 -0300

    Tratamento de falhas no back e alguns logs

--- sme_uniforme_apps/proponentes/api/serializers/proponente_serializer.py
+++ sme_uniforme_apps/proponentes/api/serializers/proponente_serializer.py
@@ -1,12 +1,17 @@
+import logging
+
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
-from ...api.serializers.anexo_serializer import AnexoSerializer
-from ...api.serializers.loja_serializer import LojaSerializer, LojaCreateSerializer
-from ...api.serializers.oferta_de_uniforme_serializer import (OfertaDeUniformeSerializer,
-                                                              OfertaDeUniformeCreateSerializer)
-from ...models import Proponente
 from ....core.models import LimiteCategoria, Uniforme
+from ...api.serializers.anexo_serializer import AnexoSerializer
+from ...api.serializers.loja_serializer import (LojaCreateSerializer,
+                                                LojaSerializer)
+from ...api.serializers.oferta_de_uniforme_serializer import (
+    OfertaDeUniformeCreateSerializer, OfertaDeUniformeSerializer)
+from ...models import Proponente
+
+log = logging.getLogger(__name__)
 
 
 class ProponenteSerializer(serializers.ModelSerializer):
@@ -71,31 +76,49 @@ class ProponenteCreateSerializer(serializers.ModelSerializer):
         ofertas_de_uniformes = validated_data.pop('ofertas_de_uniformes')
         lojas = validated_data.pop('lojas')
 
-        proponente = Proponente.objects.create(**validated_data)
+        if not ofertas_de_uniformes:
+            msgError = "Pelo menos um oferta deve ser enviada!"
+            log.debug(msgError)
+            raise ValidationError(msgError)
+
+        if not lojas:
+            msgError = "Pelo menos uma loja precisa ser enviada!"
+            log.debug(msgError)
+            raise ValidationError(msgError)
 
         categoria_acima_limite = self.categoria_acima_limite(ofertas_de_uniformes)
+        log.debug("Categoria acima do limite: {}".format(categoria_acima_limite))
         if categoria_acima_limite:
+            log.debug("Categoria acima do limite!")
             raise ValidationError(
                 f'Valor total da categoria {Uniforme.CATEGORIA_NOMES[categoria_acima_limite["categoria"]]} '
                 f'está acima do limite de R$ {categoria_acima_limite["limite"]:.2f}.')
 
         categoria_faltando_itens = self.categoria_faltando_itens(ofertas_de_uniformes)
+        log.debug("Categoria faltando itens: {}".format(categoria_acima_limite))
         if categoria_faltando_itens:
+            log.debug("Categoria com itens faltando!")
             raise ValidationError(
                 f'Não foram fornecidos todos os itens da categoria {Uniforme.CATEGORIA_NOMES[categoria_faltando_itens]}'
                 f'. Não é permitido o fornecimento parcial de uma categoria.')
+
+        proponente = Proponente.objects.create(**validated_data)
+        log.debug("Criando proponente com uuid: {}".format(proponente.uuid))
 
         ofertas_lista = []
         for oferta in ofertas_de_uniformes:
             oferta_object = OfertaDeUniformeCreateSerializer().create(oferta)
             ofertas_lista.append(oferta_object)
         proponente.ofertas_de_uniformes.set(ofertas_lista)
+        log.debug("Proponente {}, Ofertas de uniformes: {}".format(proponente.uuid, ofertas_lista))
 
         lojas_lista = []
         for loja in lojas:
             loja_object = LojaCreateSerializer().create(loja)
             lojas_lista.append(loja_object)
         proponente.lojas.set(lojas_lista)
+        log.debug("Proponente {}, lojas: {}".format(proponente.uuid, ofertas_lista))
+        log.debug("Criação de proponente finalizada!")
 
         return proponente
 
