85a3c035f3d7feaa4a26e1c69919aef695416a90
Kelwy Oliveira <kelwyduarte@gmail.com>
2020-03-26 18:01:25 -0300

    Resolve problema de salvar lat lon errado em alguns enderecos.
    Adiciona query haversine

--- sme_uniforme_apps/proponentes/services.py
+++ sme_uniforme_apps/proponentes/services.py
@@ -22,37 +22,59 @@ def cria_usuario_novo_proponente(proponente):
 
 
 def muda_status_de_proponentes(queryset, novo_status):
-     for proponente in queryset.all():
-            if proponente.status != novo_status:
-                proponente.status = novo_status
-                proponente.save()
-            if novo_status == "CREDENCIADO":
-                atualiza_coordenadas_lojas(proponente.lojas)
+    for proponente in queryset.all():
+        if proponente.status != novo_status:
+            proponente.status = novo_status
+            proponente.save()
+        if novo_status == "CREDENCIADO":
+            atualiza_coordenadas_lojas(proponente.lojas)
+
 
 def atualiza_coordenadas(queryset):
     for proponente in queryset.filter(status='CREDENCIADO').all():
         atualiza_coordenadas_lojas(proponente.lojas)
 
+
 def atualiza_coordenadas_lojas(lojas):
     log.info("Atualizando coordendas das lojas físicas")
     for loja in lojas.all():
         params = {
-            'text': f'{loja.endereco}, {loja.numero}, {loja.bairro}', 
-            'layers': LAYERS, 
+            'text': f'{loja.endereco}, {loja.numero}, {loja.bairro}, {loja.cep}',
+            'layers': LAYERS,
             'boundary.gid': BUNDARY}
         try:
             log.info(f"Buscando coordenadas: {params}")
             response = requests.get(API_URL, params=params)
+            log.info(f"retorno da api: {response.json()}")
             loja.latitude, loja.longitude = busca_latitude_e_longitude(response.json())
             loja.save()
         except Exception as e:
             log.info(f"Erro ao acessar georef.sme API: {e.__str__()}")
-        
+
 
 def busca_latitude_e_longitude(payload):
     if not payload['features']:
         raise Exception(f"API não retornou dados válidos: {payload}")
-    
+
     # A georef.sme API retorna longitude e latitude
     # mas o retorno será latitude e longitude 
-    return payload['features'][0]['geometry']['coordinates'][::-1] 
+    return payload['features'][0]['geometry']['coordinates'][::-1]
+
+
+def haversine(lat, lon):
+    """
+    Formula haversine para buscar as lojas ordenando pela distancia.
+    Para limitar os resultados a partir de uma distancia, descomentar a linha do where.
+    """
+    return f"""
+        SELECT id
+            FROM ( SELECT
+                    id,
+                    111.045 * DEGREES(ACOS(COS(RADIANS({lat}))
+                    * COS(RADIANS(latitude))
+                    * COS(RADIANS(longitude) - RADIANS({lon})) + SIN(RADIANS({lat}))
+                    * SIN(RADIANS(latitude)))) AS distance_in_km
+                 FROM proponentes_loja
+                 ORDER BY distance_in_km) as distancias
+--             WHERE distancias.distance_in_km <= 10
+            """
