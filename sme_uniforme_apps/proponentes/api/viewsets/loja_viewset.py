725f79c0a40b097e3166418d394400ea074010b0
Kelwy <kelwyduarte@gmail.com>
2020-08-12 14:23:10 -0300

    Exclui do filtro lojas que não possuem lat lon.

--- sme_uniforme_apps/proponentes/api/viewsets/loja_viewset.py
+++ sme_uniforme_apps/proponentes/api/viewsets/loja_viewset.py
@@ -24,7 +24,8 @@ class LojaViewSet(mixins.ListModelMixin, GenericViewSet):
     permission_classes = [AllowAny]
 
     def get_queryset(self):
-        queryset = Loja.objects.filter(proponente__status=Proponente.STATUS_CREDENCIADO)
+        queryset = Loja.objects.filter(proponente__status=Proponente.STATUS_CREDENCIADO).exclude(
+            latitude__isnull=True, longitude__isnull=True).exclude(latitude__exact='', longitude__exact='')
         latitude = self.request.query_params.get('latitude', None)
         longitude = self.request.query_params.get('longitude', None)
         if latitude and longitude:
