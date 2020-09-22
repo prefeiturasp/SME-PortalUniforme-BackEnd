098696de448aaa65b9e0bcccc137879042ba934c
Calvin Rossinhole <calvin.masters@gmail.com>
2020-09-22 18:00:39 -0300

    pequenos ajustes

--- sme_uniforme_apps/proponentes/api/serializers/oferta_de_uniforme_serializer.py
+++ sme_uniforme_apps/proponentes/api/serializers/oferta_de_uniforme_serializer.py
@@ -7,12 +7,16 @@ from ....core.models.uniforme import Uniforme
 class OfertaDeUniformeSerializer(serializers.ModelSerializer):
     uniforme_categoria = serializers.SerializerMethodField()
     uniforme_quantidade = serializers.SerializerMethodField()
+    nome = serializers.SerializerMethodField()
 
     def get_uniforme_categoria(self, obj):
         return obj.uniforme.categoria
 
     def get_uniforme_quantidade(self, obj):
         return obj.uniforme.quantidade
+
+    def get_nome(self, obj):
+        return obj.uniforme.nome
 
     class Meta:
         model = OfertaDeUniforme
