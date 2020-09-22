618dfa0e04fedb68fe0501ebb3c71cae448b272b
Kelwy <kelwyduarte@gmail.com>
2020-09-22 17:39:58 -0300

    Adiciona action para atualizacão de lojas

--- sme_uniforme_apps/proponentes/api/viewsets/proponentes_viewset.py
+++ sme_uniforme_apps/proponentes/api/viewsets/proponentes_viewset.py
@@ -1,14 +1,21 @@
+import logging
+
 from django_filters import rest_framework as filters
 from rest_framework import mixins, status
 from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
+from sme_uniforme_apps.proponentes.api.serializers.loja_serializer import LojaCreateSerializer
+from sme_uniforme_apps.proponentes.services import atualiza_coordenadas_lojas
 from ..serializers.proponente_serializer import ProponenteSerializer, ProponenteCreateSerializer
 
-from ...models import Proponente, ListaNegra
+from ...models import Proponente, ListaNegra, Loja
+
+log = logging.getLogger(__name__)
 
 
 class ProponentesViewSet(mixins.CreateModelMixin,
@@ -34,6 +41,48 @@ class ProponentesViewSet(mixins.CreateModelMixin,
             return ProponenteSerializer
         else:
             return ProponenteCreateSerializer
+
+    @action(detail=True, methods=['patch'], url_path='atualiza-lojas')
+    def atualiza_lojas(self, request, uuid):
+        proponente = self.get_object()
+        lojas = request.data.pop('lojas')
+        if not lojas:
+            msgError = "Pelo menos uma loja precisa ser enviada!"
+            log.info(msgError)
+            raise ValidationError(msgError)
+        lojas_ids = []
+        for loja in lojas:
+            if loja.get('id', ''):
+                lojas_ids.append(loja.get('id'))
+                loja_obj = Loja.objects.get(id=loja.get('id', ''))
+                loja_obj.numero = loja.get('numero')
+                loja_obj.bairro = loja.get('bairro')
+                loja_obj.cidade = loja.get('cidade')
+                loja_obj.complemento = loja.get('complemento')
+                loja_obj.logradouro = loja.get('logradouro')
+                loja_obj.uf = loja.get('uf')
+                loja_obj.nome_fantasia = loja.get('nome_fantasia')
+                loja_obj.telefone = loja.get('telefone')
+                loja_obj.save()
+            else:
+                atributos_extras = ['proponente', 'uuid', 'id', 'email', 'criado_em',
+                                    'alterado_em', 'latitude', 'longitude', 'cidade',
+                                    'uf', 'firstName']
+                for attr in atributos_extras:
+                    loja.pop(attr, '')
+                loja_object = LojaCreateSerializer().create(loja)
+                proponente.lojas.add(loja_object)
+                lojas_ids.append(loja_object.id)
+        atualiza_coordenadas_lojas(proponente.lojas)
+
+        for loja in proponente.lojas.all():
+            if loja.id not in lojas_ids:
+                proponente.lojas.remove(loja)
+
+        proponente.status = Proponente.STATUS_ALTERADO
+        proponente.save()
+
+        return Response(ProponenteSerializer(proponente).data, status=status.HTTP_200_OK)
 
     @action(detail=False, url_path='verifica-cnpj')
     def verifica_cnpj(self, request):
