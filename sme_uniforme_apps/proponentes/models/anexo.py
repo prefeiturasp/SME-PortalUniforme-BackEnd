558aadb9b074335f692812cda402921c8e236fc1
Kelwy <kelwyduarte@gmail.com>
2020-09-24 14:13:07 -0300

    Altera modelagem para permitir justificativas de irregularidades nos anexos

--- sme_uniforme_apps/proponentes/models/anexo.py
+++ sme_uniforme_apps/proponentes/models/anexo.py
@@ -8,6 +8,26 @@ from .tipo_documento import TipoDocumento
 
 
 class Anexo(ModeloBase):
+    # Status Choice
+    STATUS_APROVADO = 'APROVADO'
+    STATUS_REPROVADO = 'REPROVADO'
+    STATUS_PENDENTE = 'PENDENTE'
+    STATUS_VENCIDO = 'VENCIDO'
+
+    STATUS_NOMES = {
+        STATUS_APROVADO: 'Aprovado',
+        STATUS_REPROVADO: 'Reprovado',
+        STATUS_PENDENTE: 'Pendente',
+        STATUS_VENCIDO: 'Vencido'
+    }
+
+    STATUS_CHOICES = (
+        (STATUS_APROVADO, STATUS_NOMES[STATUS_APROVADO]),
+        (STATUS_REPROVADO, STATUS_NOMES[STATUS_REPROVADO]),
+        (STATUS_PENDENTE, STATUS_NOMES[STATUS_PENDENTE]),
+        (STATUS_VENCIDO, STATUS_NOMES[STATUS_VENCIDO]),
+    )
+
     historico = AuditlogHistoryField()
 
     proponente = models.ForeignKey(Proponente, on_delete=models.CASCADE, blank=True, null=True, related_name='anexos')
@@ -15,6 +35,15 @@ class Anexo(ModeloBase):
     arquivo = models.FileField()
 
     data_validade = models.DateField(null=True, blank=True)
+
+    status = models.CharField(
+        'status',
+        max_length=15,
+        choices=STATUS_CHOICES,
+        default=STATUS_PENDENTE
+    )
+
+    justificativa = models.TextField('Justificativa', blank=True, null=True)
 
     tipo_documento = models.ForeignKey(
         TipoDocumento,
