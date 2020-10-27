e6f7a8fe571e2fffc2871ab75f4d51acbaf40824
Kelwy <kelwyduarte@gmail.com>
2020-10-27 11:17:35 -0300

    Adiciona novas categorias na modelagem

--- sme_uniforme_apps/core/models/limite_categoria.py
+++ sme_uniforme_apps/core/models/limite_categoria.py
@@ -11,9 +11,9 @@ log = logging.getLogger(__name__)
 
 class LimiteCategoria(ModeloBase):
     categoria_uniforme = models.CharField(
-        max_length=10,
+        max_length=20,
         choices=Uniforme.CATEGORIA_CHOICES,
-        default=Uniforme.CATEGORIA_MALHARIA,
+        default=Uniforme.CATEGORIA_KIT_VERAO,
         unique=True,
     )
     preco_maximo = models.DecimalField('Preço máximo', max_digits=9, decimal_places=2, default=0.00)
