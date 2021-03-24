8ad00a18ede383ca0ed6ac3d13fd071775d02e72
Kelwy <kelwyduarte@gmail.com>
2021-03-24 20:03:20 -0300

    Implementa regra de envio para documentos obrigatorios sme

--- sme_uniforme_apps/proponentes/tasks.py
+++ sme_uniforme_apps/proponentes/tasks.py
@@ -86,12 +86,36 @@ def alterar_status_documentos_vencidos():
 @celery_app.task(soft_time_limit=1000, time_limit=1200)
 def enviar_email_documentos_proximos_vencimento():
     from ..proponentes.models import Proponente
+    from ..core.models import Parametros
+
     daqui_a_5_dias = datetime.date.today() + datetime.timedelta(days=5)
-    proponentes = Proponente.objects.filter(anexos__data_validade=daqui_a_5_dias)
-    for proponente in proponentes.all():
+    proponentes_anexos_obrigatorios_sme = Proponente.objects.filter(
+        anexos__data_validade=daqui_a_5_dias, anexos__tipo_documento__obrigatorio_sme=True).filter(
+        status__in=[Proponente.STATUS_EM_ANALISE, Proponente.STATUS_CREDENCIADO, Proponente.STATUS_APROVADO]
+    ).distinct()
+
+    proponentes_anexos = Proponente.objects.filter(
+        anexos__data_validade=daqui_a_5_dias, anexos__tipo_documento__obrigatorio_sme=False).filter(
+        status__in=[Proponente.STATUS_EM_ANALISE, Proponente.STATUS_CREDENCIADO, Proponente.STATUS_APROVADO]
+    ).distinct()
+
+    log.info("Proponentes com anexos próximos do fim da validade: %s", proponentes_anexos.count())
+
+    for proponente in proponentes_anexos.all():
         enviar_email_html(
             'Documento(s) próximo(s) do vencimento',
             'email_documentos_proximos_vencimento',
             None,
             proponente.email
+        )
+
+    log.info("Proponentes com anexos obrigatórios sme próximos do fim da validade: %s",
+             proponentes_anexos_obrigatorios_sme.count())
+    email_sme = Parametros.objects.first().email_sme if Parametros.objects.first() else ''
+    for proponente in proponentes_anexos_obrigatorios_sme.all():
+        enviar_email_html(
+            f'[Uniformes] Documento(s) próximo(s) do vencimento para o protocolo {proponente.protocolo}',
+            'email_documentos_proximos_vencimento_nucleo',
+            {"proponente": f'{proponente.razao_social} - {proponente.cnpj}'},
+            email_sme
         )
