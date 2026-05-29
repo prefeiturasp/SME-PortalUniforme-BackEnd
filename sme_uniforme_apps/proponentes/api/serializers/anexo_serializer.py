fb98ad0ed515ceeb00a696cd475bb20bcfadaba4
matheussacg <matheus.sacg@gmail.com>
2026-05-29 15:25:39 -0300

    feat: persiste campos de análise de IA nos anexos

--- sme_uniforme_apps/proponentes/api/serializers/anexo_serializer.py
+++ sme_uniforme_apps/proponentes/api/serializers/anexo_serializer.py
@@ -15,7 +15,18 @@ class AnexoSerializer(ModelSerializer):
 
     class Meta:
         model = Anexo
-        fields = '__all__'
+        fields = (
+            "id",
+            "criado_em",
+            "alterado_em",
+            "uuid",
+            "proponente",
+            "arquivo",
+            "data_validade",
+            "status",
+            "justificativa",
+            "tipo_documento",
+        )
 
 
 class AnexoCreateSerializer(serializers.ModelSerializer):
@@ -43,4 +54,16 @@ class AnexoCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Anexo
-        exclude = ('id',)
+        exclude = (
+            "id",
+            "status_ia",
+            "justificativa_ia",
+            "ultima_alteracao_admin_por",
+            "ultima_alteracao_admin_em",
+        )
+        extra_kwargs = {
+            'arquivo': {
+                'label': 'Documento do proponente',
+                'help_text': 'Envie apenas arquivos PDF.',
+            }
+        }
